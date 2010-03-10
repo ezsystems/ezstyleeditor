5fcd515d4f5c62dff14b729346df0c273f60b207
Nicolas Pastorino <nfrp@ez.no>
2010-03-10 11:31:29 +0000

    --Brazilian translation, credits go to Renan Leme
    git-svn-id: http://svn.projects.ez.no/ezstyleeditor/trunk@9 d2d7de43-c51e-4089-95a2-d0023ac29ad7

---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,223 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>design/ezwebin/datatype/edit/ezcssstyle</name>
+    <message>
+        <source>New CSS rule</source>
+        <translation>Nova regra de CSS</translation>
+    </message>
+    <message>
+        <source>Alias:</source>
+        <translation>Apelido:</translation>
+    </message>
+    <message>
+        <source>Selector:</source>
+        <translation>Seletor:</translation>
+    </message>
+    <message>
+        <source>Create style</source>
+        <translation>Criar estilo</translation>
+    </message>
+    <message>
+        <source>Add property</source>
+        <translation>Adicionar propriedade</translation>
+    </message>
+    <message>
+        <source>Property:</source>
+        <translation>Propriedade:</translation>
+    </message>
+    <message>
+        <source>Style:</source>
+        <translation>Estilo:</translation>
+    </message>
+    <message>
+        <source>Add</source>
+        <translation>Adicionar</translation>
+    </message>
+    <message>
+        <source>Move up.</source>
+        <translation>Mover para cima.</translation>
+    </message>
+    <message>
+        <source>Move down.</source>
+        <translation>Mover para baixo.</translation>
+    </message>
+    <message>
+        <source>Choose image</source>
+        <translation>Escolher imagem</translation>
+    </message>
+    <message>
+        <source>Remove image</source>
+        <translation>Remover imagem</translation>
+    </message>
+    <message>
+        <source>Remove properties</source>
+        <translation>Remover propriedade</translation>
+    </message>
+    <message>
+        <source>Remove rules</source>
+        <translation>Remover regras</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>Exportar</translation>
+    </message>
+    <message>
+        <source>Format:</source>
+        <translation>Formato:</translation>
+    </message>
+    <message>
+        <source>XML</source>
+        <translation>XML</translation>
+    </message>
+    <message>
+        <source>CSS</source>
+        <translation>CSS</translation>
+    </message>
+    <message>
+        <source>JSON</source>
+        <translation>JSON</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/datatype/edit/ezcssstyle_images</name>
+    <message>
+        <source>Current image</source>
+        <translation>Imagem atual</translation>
+    </message>
+    <message>
+        <source>No image selected.</source>
+        <translation>Nenhuma imagem selecionada.</translation>
+    </message>
+    <message>
+        <source>Available images</source>
+        <translation>Imagens disponíveis</translation>
+    </message>
+    <message>
+        <source>Set</source>
+        <translation>Definir</translation>
+    </message>
+    <message>
+        <source>Upload new image</source>
+        <translation>Enviar nova imagem</translation>
+    </message>
+    <message>
+        <source>Name:</source>
+        <translation>Nome:</translation>
+    </message>
+    <message>
+        <source>File:</source>
+        <translation>Arquivo:</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>Enviar</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Voltar</translation>
+    </message>
+</context>
+<context>
+    <name>design/standard/ezcssstyle</name>
+    <message>
+        <source>Add</source>
+        <translation>Adicionair</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salvar</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Aplicar</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>Enviar</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Selecionar</translation>
+    </message>
+    <message>
+        <source>Elements</source>
+        <translation>Elementos</translation>
+    </message>
+    <message>
+        <source>Styles</source>
+        <translation>Estilos</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Ajuda</translation>
+    </message>
+    <message>
+        <source>Version list</source>
+        <translation>Lista de versões</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Criar</translation>
+    </message>
+    <message>
+        <source>Properties for</source>
+        <translation>Propriedades de</translation>
+    </message>
+    <message>
+        <source>Property list is empty.</source>
+        <translation>A lista de propriedades está vazía.</translation>
+    </message>
+    <message>
+        <source>Insert image</source>
+        <translation>Inserir imagens</translation>
+    </message>
+    <message>
+        <source>Image list is empty.</source>
+        <translation>A lista de imagens está vazía.</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected properties?</source>
+        <translation>Tem certeza que deseja remover as propriedades selecionadas?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected image?</source>
+        <translation>Tem certeza que deseja remover a imagem selecionada?</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Versão</translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation>Criado</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modificado</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Restaurar</translation>
+    </message>
+    <message>
+        <source>Rules for</source>
+        <translation>Regras para</translation>
+    </message>
+    <message>
+        <source>Site style list</source>
+        <translation>Estilos do site</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Sair</translation>
+    </message>
+</context>
+</TS>
