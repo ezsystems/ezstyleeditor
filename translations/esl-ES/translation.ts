585848c7db19842497cae07ed348d7e5a9d9f37a
crevillo <crevillo@gmail.com>
2011-03-06 01:01:44 +0800

    localization: updated esl-ES translation.ts

--- translations/esl-ES/translation.ts
+++ translations/esl-ES/translation.ts
@@ -1,120 +1,122 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_ES">
 <context>
     <name>design/standard/datatype/edit/ezcssstyle</name>
     <message>
         <source>New CSS rule</source>
-        <translation>Nueva regla CCS</translation>
+        <translation type="obsolete">Nueva regla CCS</translation>
     </message>
     <message>
         <source>Alias:</source>
-        <translation>Alias:</translation>
+        <translation type="obsolete">Alias:</translation>
     </message>
     <message>
         <source>Selector:</source>
-        <translation>Selector:</translation>
+        <translation type="obsolete">Selector:</translation>
     </message>
     <message>
         <source>Create style</source>
-        <translation>Crear estilo</translation>
+        <translation type="obsolete">Crear estilo</translation>
     </message>
     <message>
         <source>Add property</source>
-        <translation>Añadir propiedad</translation>
+        <translation type="obsolete">Añadir propiedad</translation>
     </message>
     <message>
         <source>Property:</source>
-        <translation>Propiedad:</translation>
+        <translation type="obsolete">Propiedad:</translation>
     </message>
     <message>
         <source>Style:</source>
-        <translation>Estilo:</translation>
+        <translation type="obsolete">Estilo:</translation>
     </message>
     <message>
         <source>Add</source>
-        <translation>Añadir</translation>
+        <translation type="obsolete">Añadir</translation>
     </message>
     <message>
         <source>Move up.</source>
-        <translation>Subir.</translation>
+        <translation type="obsolete">Subir.</translation>
     </message>
     <message>
         <source>Move down.</source>
-        <translation>Bajar.</translation>
+        <translation type="obsolete">Bajar.</translation>
     </message>
     <message>
         <source>Choose image</source>
-        <translation>Seleccionar imagen</translation>
+        <translation type="obsolete">Seleccionar imagen</translation>
     </message>
     <message>
         <source>Remove image</source>
-        <translation>Eliminar imagen</translation>
+        <translation type="obsolete">Eliminar imagen</translation>
     </message>
     <message>
         <source>Remove properties</source>
-        <translation>Eliminar propiedades</translation>
+        <translation type="obsolete">Eliminar propiedades</translation>
     </message>
     <message>
         <source>Remove rules</source>
-        <translation>Eliminar reglas</translation>
+        <translation type="obsolete">Eliminar reglas</translation>
     </message>
     <message>
         <source>Export</source>
-        <translation>Exportar</translation>
+        <translation type="obsolete">Exportar</translation>
     </message>
     <message>
         <source>Format:</source>
-        <translation>Formato:</translation>
+        <translation type="obsolete">Formato:</translation>
     </message>
     <message>
         <source>XML</source>
-        <translation>XML</translation>
+        <translation type="obsolete">XML</translation>
     </message>
     <message>
         <source>CSS</source>
-        <translation>CSS</translation>
+        <translation type="obsolete">CSS</translation>
     </message>
     <message>
         <source>JSON</source>
-        <translation>JSON</translation>
+        <translation type="obsolete">JSON</translation>
     </message>
 </context>
 <context>
     <name>design/standard/datatype/edit/ezcssstyle_images</name>
     <message>
         <source>Current image</source>
-        <translation>Imagen actual</translation>
+        <translation type="obsolete">Imagen actual</translation>
     </message>
     <message>
         <source>No image selected.</source>
-        <translation>No se ha seleccionado ninguna imagen.</translation>
+        <translation type="obsolete">No se ha seleccionado ninguna imagen.</translation>
     </message>
     <message>
         <source>Available images</source>
-        <translation>Imágenes disponibles</translation>
+        <translation type="obsolete">Imágenes disponibles</translation>
     </message>
     <message>
         <source>Set</source>
-        <translation>Establecer</translation>
+        <translation type="obsolete">Establecer</translation>
     </message>
     <message>
         <source>Upload new image</source>
-        <translation>Cargar imagen nueva</translation>
+        <translation type="obsolete">Cargar imagen nueva</translation>
     </message>
     <message>
         <source>Name:</source>
-        <translation>Nombre:</translation>
+        <translation type="obsolete">Nombre:</translation>
     </message>
     <message>
         <source>File:</source>
-        <translation>Archivo:</translation>
+        <translation type="obsolete">Archivo:</translation>
     </message>
     <message>
         <source>Upload</source>
-        <translation>Cargar</translation>
+        <translation type="obsolete">Cargar</translation>
     </message>
     <message>
         <source>Back</source>
-        <translation>Anterior</translation>
+        <translation type="obsolete">Anterior</translation>
     </message>
 </context>
 <context>
@@ -182,6 +184,45 @@
     <message>
         <source>Exit</source>
         <translation>Salir</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Restaurar</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Crear</translation>
+    </message>
+    <message>
+        <source>Version list</source>
+        <translation>Lista de versiones</translation>
+    </message>
+    <message>
+        <source>Rules for</source>
+        <translation>Reglas para</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Versión</translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation>Creado</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modificado</translation>
+    </message>
+    <message>
+        <source>Site style list</source>
+        <translation>Lista de estilos del sitio</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezstyleeditor</name>
+    <message>
+        <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
+        <translation>El repositorio de imágenes para la ruta / node ID dada no existe. Por favor, comprueba el fichero de configuración ezstyleeditor.ini.</translation>
     </message>
 </context>
 </TS>
