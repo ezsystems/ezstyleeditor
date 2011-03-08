048abc2cac271341d89bd29dc766dafacf98a95c
Damien Pobel <dpobel@free.fr>
2011-03-08 10:07:06 +0100

    Localization: updated base translations.ts file

--- translations/untranslated/translation.ts
+++ translations/untranslated/translation.ts
@@ -1,111 +1,26 @@
-<!DOCTYPE TS><TS>
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
 <context>
-    <name>design/ezwebin/datatype/edit/ezcssstyle</name>
-    <message>
-        <source>New CSS rule</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Alias:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Selector:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Create style</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Add property</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Property:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Style:</source>
-        <translation type="unfinished"></translation>
-    </message>
+    <name>design/standard/ezcssstyle</name>
     <message>
         <source>Add</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Move up.</source>
+        <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Move down.</source>
+        <source>Save</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Choose image</source>
+        <source>Apply</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Remove image</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Remove properties</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Remove rules</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Export</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Format:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>XML</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>CSS</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>JSON</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>design/ezwebin/datatype/edit/ezcssstyle_images</name>
-    <message>
-        <source>Current image</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>No image selected.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Available images</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Set</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Upload new image</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Name:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>File:</source>
+        <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -113,7 +28,82 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Back</source>
+        <source>Choose</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Elements</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Properties for</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Property list is empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Image list is empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected properties?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected image?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Rules for</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Site style list</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezstyleeditor</name>
+    <message>
+        <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
