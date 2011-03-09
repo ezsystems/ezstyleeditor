54b7b02d4a060308899082a62363477b56e95ab4
Eric Sagnes <es@ez.no>
2011-03-09 10:33:48 +0800

    localization: updated jpn-JP translation.ts

---
+++ translations/jpn-JP/translation.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>design/standard/ezcssstyle</name>
+    <message>
+        <source>Add</source>
+        <translation>追加</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>適用</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>アップロード</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>選択</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>復元</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>作成</translation>
+    </message>
+    <message>
+        <source>Version list</source>
+        <translation>バージョン一覧</translation>
+    </message>
+    <message>
+        <source>Elements</source>
+        <translation>要素</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>ヘルプ</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>終了</translation>
+    </message>
+    <message>
+        <source>Properties for</source>
+        <translation>への属性</translation>
+    </message>
+    <message>
+        <source>Property list is empty.</source>
+        <translation>属性リストは空です。</translation>
+    </message>
+    <message>
+        <source>Insert image</source>
+        <translation>画像挿入</translation>
+    </message>
+    <message>
+        <source>Image list is empty.</source>
+        <translation>画像リストは空です。</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected properties?</source>
+        <translation>選択された属性を削除してもよろしいですか？</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected image?</source>
+        <translation>選択された画像を削除してもよろしいですか？</translation>
+    </message>
+    <message>
+        <source>Rules for</source>
+        <translation>へのルール</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>バージョン</translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation>作成日時</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>変更日時</translation>
+    </message>
+    <message>
+        <source>Site style list</source>
+        <translation>サイトスタイル一覧</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezstyleeditor</name>
+    <message>
+        <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
+        <translation>設定したパス/ノードIDに画像リポジトリが存在しません。ezstyleeditor.ini設定ファイルを確認してください。</translation>
+    </message>
+</context>
+</TS>
