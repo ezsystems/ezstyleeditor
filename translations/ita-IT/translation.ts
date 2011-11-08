49ae1ea34a6f0604b33b99897c753fad8f57c316
Sandra Parente <sandra@netbliss.it>
2011-11-08 18:29:58 +0100

    Translated in italian

---
+++ translations/ita-IT/translation.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>design/standard/ezcssstyle</name>
+    <message>
+        <source>Add</source>
+        <translation>Aggiungi</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Elimina</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Registra</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Applica</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>Carica</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Scegli</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Recupera</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Crea</translation>
+    </message>
+    <message>
+        <source>Version list</source>
+        <translation>Elenco versioni</translation>
+    </message>
+    <message>
+        <source>Elements</source>
+        <translation>Elementi</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Aiuto</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>Properties for</source>
+        <translation>Proprietà per</translation>
+    </message>
+    <message>
+        <source>Property list is empty.</source>
+        <translation>L&apos;elenco delle proprietà è vuoto.</translation>
+    </message>
+    <message>
+        <source>Insert image</source>
+        <translation>Inserisci immagine</translation>
+    </message>
+    <message>
+        <source>Image list is empty.</source>
+        <translation>L&apos;elenco delle immagini è vuoto.</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected properties?</source>
+        <translation>Vuoi davvero eliminare le proprietà selezionate?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected image?</source>
+        <translation>Vuoi davvero eliminare le immagini selezionate?</translation>
+    </message>
+    <message>
+        <source>Rules for</source>
+        <translation>Regole per</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Versione</translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation>Creato</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Modificato</translation>
+    </message>
+    <message>
+        <source>Site style list</source>
+        <translation>Elenco stili del sito</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezstyleeditor</name>
+    <message>
+        <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
+        <translation>L&apos;archivio delle immagini per il percorso / ID nodo fornito non esiste. Controlla la configurazione dell&apos;INI file ezstyleeditor.ini.</translation>
+    </message>
+</context>
+</TS>
