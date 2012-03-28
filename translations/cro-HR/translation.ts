aaf793aa91a2baa77ad7f5026fc662d61152d88f
Edi Modric <edi.modric@gmail.com>
2012-03-28 09:15:57 +0000

    Add Croatian translation

---
+++ translations/cro-HR/translation.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>design/standard/ezcssstyle</name>
+    <message>
+        <source>Add</source>
+        <translation>Dodaj</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Izbaci</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Spremi</translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation>Primijeni</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Odustani</translation>
+    </message>
+    <message>
+        <source>Upload</source>
+        <translation>Pošalji</translation>
+    </message>
+    <message>
+        <source>Choose</source>
+        <translation>Odeberi</translation>
+    </message>
+    <message>
+        <source>Restore</source>
+        <translation>Vrati nazad</translation>
+    </message>
+    <message>
+        <source>Create</source>
+        <translation>Kreiraj</translation>
+    </message>
+    <message>
+        <source>Version list</source>
+        <translation>Lista verzija</translation>
+    </message>
+    <message>
+        <source>Elements</source>
+        <translation>Elementi</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Pomoć</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Izlaz</translation>
+    </message>
+    <message>
+        <source>Properties for</source>
+        <translation>Svojstva za</translation>
+    </message>
+    <message>
+        <source>Property list is empty.</source>
+        <translation>Lista svojstava je prazna.</translation>
+    </message>
+    <message>
+        <source>Insert image</source>
+        <translation>Ubaci sliku</translation>
+    </message>
+    <message>
+        <source>Image list is empty.</source>
+        <translation>Lista slika je prazna.</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected properties?</source>
+        <translation>Jeste li sigurni da želite ukloniti odabrana svojstva?</translation>
+    </message>
+    <message>
+        <source>Do you really want to remove selected image?</source>
+        <translation>Jeste li sigurni da želite ukloniti odabranu sliku?</translation>
+    </message>
+    <message>
+        <source>Rules for</source>
+        <translation>Pravila za</translation>
+    </message>
+    <message>
+        <source>Version</source>
+        <translation>Verzija</translation>
+    </message>
+    <message>
+        <source>Created</source>
+        <translation>Objavljeno</translation>
+    </message>
+    <message>
+        <source>Modified</source>
+        <translation>Promijenjeno</translation>
+    </message>
+    <message>
+        <source>Site style list</source>
+        <translation>Lista stilova</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezstyleeditor</name>
+    <message>
+        <source>Image repository for given path / node ID does not exist. Please check ezstyleeditor.ini INI file configuration.</source>
+        <translation>Repozitorij slika za odabrani čvor ne postoji. Provjerite konfiguraciju u datoteci ezstyleeditor.ini.</translation>
+    </message>
+</context>
+</TS>
