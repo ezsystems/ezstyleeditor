6036575f62e676831e42f25eaa646fa347281b38
Lukasz Serwatka <l.serwatka@gazeta.pl>
2012-07-06 11:26:42 +0200

    Updated DB schema

--- sql/mysql/mysql.sql
+++ sql/mysql/mysql.sql
@@ -9,17 +9,31 @@ CREATE TABLE IF NOT EXISTS `ezcssesitestyle` (
 
 CREATE TABLE IF NOT EXISTS `ezcssesitestyle_definition` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sitestyle_id` int(11) NOT NULL DEFAULT 0,
+  `sitestyle_id` int(11) NOT NULL DEFAULT '0',
   `style` text,
-  `version` int(11) NOT NULL DEFAULT 0,
+  `version` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ezcssesitestyle_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `ezcssesitestyle_group_link` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sitestyle_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ezcssesitestyle_gl_sitestyle_id_group_id` (`sitestyle_id`,`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `ezcssesitestyle_version` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `version` int(11) NOT NULL DEFAULT 0,
-  `sitestyle_id` int(11) NOT NULL DEFAULT 0,
-  `created` int(11) NOT NULL DEFAULT 0,
-  `modified` int(11) NOT NULL DEFAULT 0,
+  `version` int(11) NOT NULL DEFAULT '0',
+  `sitestyle_id` int(11) NOT NULL DEFAULT '0',
+  `created` int(11) NOT NULL DEFAULT '0',
+  `modified` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
